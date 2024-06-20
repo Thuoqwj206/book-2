a95eea1fcb4b6dc8b9dfe6a9cb952cf73eefbcba
luongtvDEV <luongtv.developer@gmail.com>
2024-06-20 23:31:42 +0700

    hello

--- js/function.js
+++ js/function.js
@@ -61,4 +61,28 @@
     }
   }
 
+  document.addEventListener("mouseup", highlightText);
 
+  function highlightSelectedText() {
+    var selectedText = window.getSelection().toString();
+    if (selectedText !== "") {
+      var cardContainer = document.getElementById("content");
+      highlightTextInElement(cardContainer, selectedText);
+    }
+  }
+  
+  function highlightTextInElement(element, selectedText) {
+    var children = element.childNodes;
+    for (var i = 0; i < children.length; i++) {
+      var child = children[i];
+      if (child.nodeType === 3) { // Text node
+        var text = child.textContent;
+        var highlightedText = text.replace(new RegExp(selectedText, "gi"), '<span class="highlight">$&</span>');
+        var wrapper = document.createElement("span");
+        wrapper.innerHTML = highlightedText;
+        element.replaceChild(wrapper, child);
+      } else if (child.nodeType === 1) { // Element node
+        highlightTextInElement(child, selectedText);
+      }
+    }
+  }
