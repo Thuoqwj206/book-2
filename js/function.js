1538cf824f18bdbfe4cecba05edd9b3d866f17c0
luongtvDEV <luongtv.developer@gmail.com>
2024-06-21 10:37:39 +0700

    update highlight

--- js/function.js
+++ js/function.js
@@ -61,28 +61,82 @@
     }
   }
 
-  document.addEventListener("mouseup", highlightText);
+  // document.addEventListener("mouseup", highlightText);
 
-  function highlightSelectedText() {
+  // function highlightSelectedText() {
+  //   var selectedText = window.getSelection().toString();
+  //   if (selectedText !== "") {
+  //     var cardContainer = document.getElementById("content");
+  //     highlightTextInElement(cardContainer, selectedText);
+  //   }
+  // }
+  
+  // function highlightTextInElement(element, selectedText) {
+  //   var children = element.childNodes;
+  //   for (var i = 0; i < children.length; i++) {
+  //     var child = children[i];
+  //     if (child.nodeType === 3) { // Text node
+  //       var text = child.textContent;
+  //       var highlightedText = text.replace(new RegExp(selectedText, "gi"), '<span class="highlight">$&</span>');
+  //       var wrapper = document.createElement("span");
+  //       wrapper.innerHTML = highlightedText;
+  //       element.replaceChild(wrapper, child);
+  //     } else if (child.nodeType === 1) { // Element node
+  //       highlightTextInElement(child, selectedText);
+  //     }
+  //   }
+  // }
+  function toggleHighlight() {
     var selectedText = window.getSelection().toString();
     if (selectedText !== "") {
-      var cardContainer = document.getElementById("content");
-      highlightTextInElement(cardContainer, selectedText);
+      if (hasHighlight(selectedText)) {
+        removeHighlight(selectedText);
+      } else {
+        highlightSelectedText(selectedText);
+      }
     }
   }
   
-  function highlightTextInElement(element, selectedText) {
+  function highlightSelectedText(selectedText) {
+    var cardContainer = document.getElementById("content");
+    highlightTextInElement(cardContainer, selectedText, true);
+  }
+  
+  function highlightTextInElement(element, selectedText, removeExistingHighlights) {
     var children = element.childNodes;
     for (var i = 0; i < children.length; i++) {
       var child = children[i];
       if (child.nodeType === 3) { // Text node
         var text = child.textContent;
-        var highlightedText = text.replace(new RegExp(selectedText, "gi"), '<span class="highlight">$&</span>');
+        var highlightedText = text;
+        if (removeExistingHighlights) {
+          highlightedText = highlightedText.replace(/<span class="highlight">|<\/span>/gi, '');
+        }
+        highlightedText = highlightedText.replace(new RegExp(selectedText, "gi"), '<span class="highlight">$&</span>');
         var wrapper = document.createElement("span");
         wrapper.innerHTML = highlightedText;
         element.replaceChild(wrapper, child);
       } else if (child.nodeType === 1) { // Element node
-        highlightTextInElement(child, selectedText);
+        highlightTextInElement(child, selectedText, removeExistingHighlights);
       }
     }
   }
+  
+  function removeHighlight() {
+    var highlightedText = document.getElementsByClassName("highlight");
+    while (highlightedText.length) {
+      var parent = highlightedText[0].parentNode;
+      parent.replaceChild(document.createTextNode(highlightedText[0].textContent), highlightedText[0]);
+    }
+  }
+  
+  function hasHighlight(selectedText) {
+    var highlightedText = document.getElementsByClassName("highlight");
+    for (var i = 0; i < highlightedText.length; i++) {
+      if (highlightedText[i].textContent === selectedText) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
